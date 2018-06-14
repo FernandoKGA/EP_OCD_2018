cb3cc49ce092c7074aa53768f0df0dec69146e55
Lucas <lucas.pcbranco@hotmail.com>
2018-06-14 20:03:08 -0300

    Novo codigo em C com ambas versoes

--- EP/ocd.c
+++ EP/ocd.c
@@ -35,13 +35,11 @@ void verificando(int c){
 }
 
 void converte(char ch, int *tipo, char *valor){
-    /*
-        O tipo 'char' eh na verdade uma representacao de um
-        inteiro, transformado para caracter, exemplos: 
+    /* O tipo 'char' eh na verdade uma representacao de um
+        inteiro, transformado para caracter, exemplos:
         57 -> '9'
         48 -> '0'
-        97 -> 'a'
-    */
+        97 -> 'a'*/
     int caracter = (int) ch;
 
     //if(ch) inteiro retorna tipo 0
@@ -53,11 +51,10 @@ void converte(char ch, int *tipo, char *valor){
         //if(ch) letra(M ou m) retorna tipo 1
         /*
             No caso de ser letra, converte para maiuscula,
-            se nao devolve inalterado.
-        */
+            se nao devolve inalterado. */
         if((caracter >= 65 && caracter <= 90) || ( //Letra
             caracter >= 97 && caracter <= 122)){
-            
+
             *tipo = 1;
             if(caracter >= 97 && caracter <= 122){ //Se for minuscula
                 /*
@@ -73,8 +70,34 @@ void converte(char ch, int *tipo, char *valor){
             *tipo = 2;
             *valor = ch;
         }
-            
-    } 
+
+    }
+}
+
+void converte(char ch, int *tipo, char *valor){
+    //converte de char para inteiro
+    int c = (int) ch;
+    //garante que valor receba o valor de ch
+    *valor = ch;
+    //verifica se é alfanumérico
+    if(isalnum(c)){
+        if(isdigit(c)){
+            //é um número inteiro, logo retorna o tipo 0
+            *tipo = 0;
+            return;
+        }else{
+            //é uma letra, logo, retorna o tipo 1
+            *tipo = 1;
+            if(islower(c)){
+                *valor = toupper(c);
+            }
+            return;
+        }
+    }else{
+        //não é um número nem uma letra, logo, retorna o tipo 2
+        *tipo = 2;
+        return;
+    }
 }
 
 void imprimeSequencia(char array[]){
@@ -84,12 +107,12 @@ void imprimeSequencia(char array[]){
         caracteres que nao forem letras ou numeros.
     */
     printf("Imprime sequencia.\n");
-    int n = strlen(array);  //uso de string requer <string.h> 
+    int n = strlen(array);  //uso de string requer <string.h>
     //Caso seja um array de caracteres separados, pode se usar sizeof().
     int tipo;
     char valor;
-    for(int i=0 ;i < n; i++){  
-        converte(array[i],&tipo,&valor);  //Converte
+    for(int i=0 ;i < n; i++){
+        converte_2(array[i],&tipo,&valor);  //Converte
         if(tipo == 1){  //Se for letra, imprime (ja convertido)
             printf("%c",valor);
         }
@@ -111,7 +134,7 @@ int main(){
     char valor;
     char car = '3';
     printf("Chamando converte.\n");
-    converte(car,&tipo,&valor);
+    converte_2(car,&tipo,&valor);
     printf("Conversao de: %c.\nTipo: %i.\nValor: %c\n",car,tipo,valor);
     char array[] = "C0onV3ER2 sa0o";
     printf(array);
