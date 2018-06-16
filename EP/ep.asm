9e5cd148523535e4f746e97486da302979290107
FernandoKGA <fernando.kga@gmail.com>
2018-06-16 18:26:28 -0300

    Ajuste no EP dado o PDF

--- EP/ep.asm
+++ EP/ep.asm
@@ -3,73 +3,74 @@
 main:
 	li $a1,0      #carrega contador apartir do 0
 	lb $a2,$tam  #carrega o tamanho da string
-	j imprime_sequencia
+	j imprime_sequencia   #vai para a funcao de imprimir sequencia
 	
-fim_programa:   #função que imprime uma sequencia de caracteres
-	li $v0, 10
-	syscall
-	
-imprime_sequencia:   #função que imprime uma sequencia de caracteres (convertendo)
-	lb $t2,$sequencia($a1)
-	move $a0, $t2               #passa o caracter para o a0 como argumento
-	addi $a1,$a1,1               #adiciona um no contador
+imprime_sequencia:   #funcao que imprime uma sequencia de caracteres (convertendo)
+	lb $t2,$sequencia($a1)      #pega o byte segundo o indice apresentado
+	move $a0, $t2               #passa o caracter para o $a0 como argumento
+	addi $a1,$a1,1               #adiciona 1 no contador
 	jal converte                    #funcao de conversao
 	jal printa_caracter      #funcao de printar caracter
-	blt $a1,$a2, imprime_sequencia   #se a1 menor que a2
+	blt $a1,$a2, imprime_sequencia   #se a1 menor que a2 faz loop
 	j fim_programa            #chama o encerramento do programa
 	
-converte:  #função que converte um único caracter
+fim_programa:   #funcao que imprime uma sequencia de caracteres
+	li $v0, 10    #codigo para sair do programa
+	syscall	  #chama o sistema operacional
+	
+converte:  #funcao que converte um unico caracter
 
 	#numero
-	sgeu $t0, $a0, 48          
-	sleu $t1, $a0, 57
-	and $t0, $t0, $t1
-	beq $t0, 1, numero  #se eh letra maiuscula entra na funcao
+	sgeu $t0, $a0, 48   #intervalo de ascii
+	sleu $t1, $a0, 57   #intervalo de ascii
+	and $t0, $t0, $t1   #se esta dentro desse intervalo  $t0 = 1 / c.c. $t0 = 0
+	beq $t0, 1, numero  #se eh numero entra na funcao
 	
 	#letra maiuscula
-	sgeu $t0, $a0, 65          
-	sleu $t1, $a0, 90
-	and $t0, $t0, $t1
+	sgeu $t0, $a0, 65   #intervalo de ascii
+	sleu $t1, $a0, 90   #intervalo de ascii
+	and $t0, $t0, $t1   #se esta dentro desse intervalo $t0 = 1 / c.c. $t0 = 0
 	beq $t0, 1, letra_maiuscula  #se eh letra maiuscula entra na funcao
 	
 	#letra minuscula
-	sgeu $t0, $a0, 97
-	sleu $t1, $a0, 122
-	and $t0, $t0, $t1
+	sgeu $t0, $a0, 97     #intervalo de ascii
+	sleu $t1, $a0, 122    #intervalo de ascii
+	and $t0, $t0, $t1     #se entra dentro desse intervalo $t0 = 1 / c.c. $t0 = 0
 	beq $t0, 1, letra_minuscula  #se eh letra minuscula entra na funcao
 	
 	#se passou direto entra para caracter especial
-	j carac_especial
+	j carac_especial  #funcao de caracter especial
 	
-converte_retorno:
-	jr $ra
+converte_retorno:   #funcao que entra para dar jump ao endereco de retorno
+	jr $ra     #da jump para a instrucao onde o registro de retorno aponta
 	
-printa_caracter:
+printa_caracter:   #funcao que faz o print dos caracteres
 	beq $t2, 2, printa_carac_retorno #se nao for letra ou numero, ja retorna sem printar
 	li $v0, 11   #printa caracter
-	syscall
-	j printa_carac_retorno
+	syscall    #chama o sistema para fazer a operacao
+	j printa_carac_retorno   #chama o retorno ao final para voltar ao loop
 	
-printa_carac_retorno:
-	jr $ra
+printa_carac_retorno:  #funcao que entra para dar jump ao endereco de retorno
+	jr $ra     #da jump para a instrucao onde o registro de retorno aponta
 	
-numero:
-	li $t2, 0
-	j converte_retorno 
+numero:      #funcao que coloca o tipo do caracter no registrador $t2
+	li $t2, 0    #coloca 0 em $t2 para numeros
+	j converte_retorno     #chama o retorno da funcao de conversao
 	
-letra_minuscula:
-	li $t2, 1
+letra_minuscula:    #funcao que coloca o tipo do caracter no registrador $t2 e converte em maiuscula
+	li $t2, 1     #coloca 1 em $t2 para letras
 	sub $a0, $a0, 32   #transforma para maiuscula subtraindo 32
-	j converte_retorno
+	j converte_retorno   #chama o retorno da funcao de conversao
 	
-letra_maiuscula:
-	li $t2, 1
-	j converte_retorno
+letra_maiuscula:   #funcao que coloca o tipo do caracter no registrador $t2
+	li $t2, 1    #coloca 1 em $t2 para letras
+	j converte_retorno   #chama o retorno da funcao de conversao
 
-carac_especial:
-	li $t2, 2
-	j converte_retorno
+carac_especial:    #funcao que coloca o tipo do caracter no registrador $t2
+	li $t2, 2    #coloca 2 em $t2 para caracteres especiais
+	j converte_retorno    #chama o retorno da funcao de conversao
 
 .data
-$tam: .word 14
+#deve ser especificado o tamanho da sequencia de caracteres em .asciiz e a sequencia em si
+$tam: .word 14        #tamanho da sequencia de caracteres
 $sequencia: .asciiz "C0oNv3eRS 4Ao"
